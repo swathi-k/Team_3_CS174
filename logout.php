94c9da453da359165e4b5cc21e615e8eb26d49b7
Swathi Kotturu <kotturuswathi@yahoo.com>
2014-12-01 18:28:24 -0800

    Logout redirection works

--- logout.php
+++ logout.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset="UTF-8" http-equiv="refresh" content="3;url=./index.php">
 <title>Logout</title>
 </head>
 <body>
 
-<?php 
+<?php
+
+
+
+ 
 	print ("<h1>You have logged out!</h1><h3>Redirecting in 3 seconds...</h3>");
 	session_start();
 	
@@ -16,9 +20,9 @@
 	
 	setcookie("uid", '', time() - 60);
 	
-	header("refresh:3;url=./index.php");
+//	header("refresh:3;url=/index.php");
+
 	
 ?>
-
 </body>
 </html>
